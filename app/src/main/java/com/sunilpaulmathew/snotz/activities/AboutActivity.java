82c24e0118fa2e79fedec15d03697b10a1c0381e
sunilpaulmathew <sunil.kde@gmail.com>
2023-03-22 21:49:08 +0100

    Simplified About page
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/sunilpaulmathew/snotz/activities/AboutActivity.java
+++ app/src/main/java/com/sunilpaulmathew/snotz/activities/AboutActivity.java
@@ -8,16 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textview.MaterialTextView;
 import com.sunilpaulmathew.snotz.BuildConfig;
 import com.sunilpaulmathew.snotz.R;
 import com.sunilpaulmathew.snotz.utils.sNotzColor;
-import com.sunilpaulmathew.snotz.utils.sNotzData;
-
-import java.util.Objects;
 
 import in.sunilpaulmathew.sCommon.CommonUtils.sCommonUtils;
-import in.sunilpaulmathew.sCommon.FileUtils.sFileUtils;
 
 /*
  * Created by sunilpaulmathew <sunil.kde@gmail.com> on October 17, 2020
@@ -33,27 +30,25 @@ public class AboutActivity extends AppCompatActivity {
         MaterialTextView mAppName = findViewById(R.id.app_title);
         MaterialTextView mMadeBy = findViewById(R.id.made_by);
         AppCompatImageView mSunil = findViewById(R.id.sunil);
-        MaterialTextView mChange_logs_title = findViewById(R.id.change_logs_title);
-        MaterialTextView mChange_logs = findViewById(R.id.change_logs);
+        MaterialCardView mCancelCard = findViewById(R.id.cancel_card);
         MaterialTextView mCancel = findViewById(R.id.cancel_button);
         LinearLayoutCompat mSourceCode = findViewById(R.id.source_code);
         LinearLayoutCompat mMoreApps = findViewById(R.id.play_store);
         LinearLayoutCompat mReportIssue = findViewById(R.id.report_issue);
 
         mAppName.setTextColor(sNotzColor.getAppAccentColor(this));
-        mCancel.setTextColor(sNotzColor.getAppAccentColor(this));
-        mChange_logs_title.setTextColor(sNotzColor.getAppAccentColor(this));
+        mCancel.setTextColor(sNotzColor.getTextColor(this));
+        mCancelCard.setCardBackgroundColor(sNotzColor.getAppAccentColor(this));
+        mCancelCard.setStrokeColor(sNotzColor.getAppAccentColor(this));
         mMadeBy.setTextColor(sNotzColor.getAppAccentColor(this));
 
         mSourceCode.setOnClickListener(v -> sCommonUtils.launchUrl("https://github.com/sunilpaulmathew/sNotz/", this));
         mMoreApps.setOnClickListener(v -> sCommonUtils.launchUrl("https://play.google.com/store/apps/dev?id=5836199813143882901", this));
         mReportIssue.setOnClickListener(v -> sCommonUtils.launchUrl("https://github.com/sunilpaulmathew/sNotz/issues/new", this));
         mSunil.setOnClickListener(v -> sCommonUtils.launchUrl("https://github.com/sunilpaulmathew", this));
-        mCancel.setOnClickListener(v -> onBackPressed());
+        mCancelCard.setOnClickListener(v -> onBackPressed());
 
         mAppName.setText(getString(R.string.app_name) + " " + BuildConfig.VERSION_NAME);
-        mChange_logs.setText(Objects.requireNonNull(sNotzData.getJSONObject(sFileUtils.readAssetFile(
-                "changelogs.json", this))).get("releaseNotes").getAsString());
     }
 
 }
